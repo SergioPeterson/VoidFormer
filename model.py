54328265451d6d619c414453167505a7befd12e1
Sergio Peterson <sergiopeter2016@berkeley.edu>
2024-12-05 15:13:46 -0800

    starting encoiding

--- model.py
+++ model.py
@@ -23,6 +23,17 @@ class PositinalEncoding(nn.Module):
         # Matrix of shape(max_length <sequnce length>, embedding_dim)
         pe = torch.zeros(max_length, embedding_dim)
         # Create a vector of shape (embedding_dim)
+        position = torch.arange(0,max_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, embedding_dim, 2).float() * (-math.log(10000.0) / embedding_dim)) # (embedding_dim / 2)
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / embedding_dim))
+        # Apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / embedding_dim))
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, embedding_dim)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
 
 
 
